e5a7ac3c38b00c3ca47abb61c481cbddc34061f0
Rick Tungol <rickizta@gmail.com>
2015-09-05 07:15:18 +0800

    edit db config

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-  "url": "mongodb://localhost/solar"
+  //"url": "mongodb://localhost/solar"
+  'url' : 'mongodb://heroku_app34882199:uv9us3j2btivac0u651q4ba5bu@ds061601.mongolab.com:61601/heroku_app34882199'
 }
